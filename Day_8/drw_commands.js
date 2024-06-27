2211f5f9229aad1ae5d8d4605b64942fe987f8e6
MiaGMB <mre42348@stud.hs-furtwangen.de>
2024-06-27 13:58:25 +0200

    added new project

---
+++ Day_8/drw_commands.js
@@ -0,0 +1,11 @@
+"use strict";
+const canvas = document.getElementsByTagName("canvas")[0];
+const ctx = canvas.getContext("2d");
+ctx.rect(10, 10, 100, 100);
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.stroke();
+ctx.fillStyle = "rgb(255, 0, 255)";
+let path = new Path2D();
+path.rect(200, 200, 50, 50);
+ctx.fill(path);
